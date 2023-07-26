1ed51e0154530765dfb54f56d8cf2a67722d7113
sangeetakuntoji <139916774+sangeetakuntoji@users.noreply.github.com>
2023-07-26 20:37:27 +0200

    Update terraform.tf
    removed RG

--- terraform.tf
+++ terraform.tf
@@ -1,15 +1,5 @@
-
-resource "azurerm_resource_group" "rg" {
-  name     = "dev-rg"
-  location = "eastus2"
-}
-
-
 resource "azurerm_static_site" "web" {
   name                = "devxyzstaticwebapp"
   resource_group_name = "dev-rg"
   location            = "eastus2"
-}
-
-
-  
+} 
