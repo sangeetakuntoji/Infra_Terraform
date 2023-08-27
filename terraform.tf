0b8cc099a79a0a9c262f4b30fe4b2da08a9dac67
sangeetakuntoji <sangeeta.kuntoji@gmail.com>
2023-08-27 19:49:10 +0200

    uptodate

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 resource "azurerm_static_site" "web" {
-  name                = "devxyzstaticwebapp"
+  name                = "devxyzstaticwebapp01"
   resource_group_name = "dev-rg"
   location            = "eastus2"
 }
